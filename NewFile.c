015fbf4af291d6aace20de32d8a8beb1e8e0ab70
Visty31 <85346723+Visty31@users.noreply.github.com>
2021-06-25 14:23:14 +0300

    Update NewFile.c

--- NewFile.c
+++ NewFile.c
@@ -1,15 +1,11 @@
+// hello world in C
 include<stdio.h>
 int main()
 {
-    printf("Size of char: %ld byte\n",sizeof(char));
-    printf("Size of int: %ld bytes\n",sizeof(int));
-    printf("Size of float: %ld bytes\n",sizeof(float));
-    printf("Size of double: %ld bytes", sizeof(double));
+    printf("Hello World\n");
+    printf("Iam Visty and i love programing ");
+    printf("Join me as i code ; coding is FUN ");
+    
     return 0;
-}
-Output:
-
-Size of char: 1 byte
-Size of int: 4 bytes
-Size of float: 4 bytes
-Size of double: 8 bytes
+    
+}    
